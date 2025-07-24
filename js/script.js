07b0c6b8d62dac839b2ada42049417b1c0ca0015
cramerjillian <cramerjillian@gmail.com>
2025-07-24 14:10:37 -0400

    First commit, initial html and javascript created

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let map;
+let resultsList;
+let GOOGLE_API_KEY="hidden";
+
+document.getElementById("input").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let city = document.getElementById("cityInput").value;
+    let state = document.getElementById("stateInput").value;
+    let distance = document.getElementById("stateInput").value;
+
+})
